7850ca7f892d1fe5ad7624b865ec3d58ae3f774b
1223haris <harismunir.034150@gmail.com>
2022-12-16 03:48:33 -0800

    new changes

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const {userControllers} = require('../controllers');
+const userControllers = require('../controllers/usersController.js');
 
 /* GET users listing. */
-router.get('/get', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
   const query = req.query;
   console.log(query)
   try{
-const results = await userControllers.getAllUser(query)
-res.status(200).send(result);
+const results = await userControllers.getAlluser(query)
+res.status(200).send(results);
 
   } catch (error) {
+    console.log(error);
     res.status(500).send(error);
 
 
@@ -18,12 +19,12 @@ res.status(200).send(result);
 
   
 });
-router.post('/addUser', async function(req, res, next) {
+router.post('/', async function(req, res, next) {
   const body = req.body;
-  console.log(body)
+  console.log(body);
   try{
         const results = await userControllers.addUser(body)
-        res.status(200).send(result);
+        res.status(200).send(results);
   } catch (error) {
     res.status(500).send(error);
 
@@ -32,5 +33,35 @@ router.post('/addUser', async function(req, res, next) {
 
   
 });
+router.put('/' , async function (req, res, next){
+   
+  const body = req.body;
+  // console.log(body);
+  if(!body._id){
+    return res.status(400).send({message : "_id is required"});
 
+  }
+  try {
+    const results = await userControllers.updateUser(body);
+    res.status(200).send(results);
+
+
+  }
+  catch (error){
+    res.status(500).send(error);
+  }
+});
+router.delete('/:id' , async function (req, res, next){
+  const id = req.params.id;
+  const filter = {_id: id};
+  try{
+    const result = await userControllers.deleteUser(filter);
+    res.status(200).send('Deleted Successfully');
+    
+  }
+  catch (error){
+    console.log(error);
+    res.status(500).send (error);
+  }
+});
 module.exports = router;
